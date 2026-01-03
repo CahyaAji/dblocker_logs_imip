651a0341b5edfd41e5b0140ea47f7c01984cfebf
CahyaAji <cahya.akk@gmail.com>
2026-01-03 22:18:37 +0700

    mqtt route

--- routes/routes.go
+++ routes/routes.go
@@ -2,19 +2,22 @@ package routes
 
 import (
 	"dblocker_logs_server/internal/handlers"
+	"dblocker_logs_server/internal/mqtt_client"
 	"dblocker_logs_server/internal/repository"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupRouter(db *gorm.DB) *gin.Engine {
+func SetupRouter(db *gorm.DB, mqttClient *mqtt_client.MqttClient) *gin.Engine {
 	r := gin.Default()
 
 	deviceRepo := repository.NewDeviceRepository(db)
 	actionLogRepo := repository.NewActionLogRepository(db)
 	deviceLogRepo := repository.NewDeviceLogRepository(db)
 	userRepo := repository.NewUserRepository(db)
+
+	commandHandler := handlers.NewCommandHandler(mqttClient, deviceRepo)
 
 	deviceHandler := handlers.NewDeviceHandler(deviceRepo)
 	actionLogHandler := handlers.NewActionLogHandler(actionLogRepo)
@@ -26,6 +29,8 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 	r.GET("/devices/:id", deviceHandler.GetDeviceByID)
 	r.PUT("/devices/:id", deviceHandler.UpdateDevice)
 	r.DELETE("/devices/:id", deviceHandler.DeleteDevice)
+
+	r.POST("/commands", commandHandler.ExecuteCommand)
 
 	r.POST("/action-logs", actionLogHandler.CreateActionLog)
 	r.GET("/action-logs", actionLogHandler.GetActionLogs)
