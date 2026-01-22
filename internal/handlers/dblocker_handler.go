6f8899d83d61ee0d28f1f4102820d9525d3dfba7
CahyaAji <cahya.akk@gmail.com>
2026-01-22 20:34:11 +0700

    add crud handler

--- internal/handlers/dblocker_handler.go
+++ internal/handlers/dblocker_handler.go
@@ -1,7 +1,102 @@
 package handlers
 
-import "dblocker_logs_server/internal/repository"
+import (
+	"dblocker_logs_server/internal/models"
+	"dblocker_logs_server/internal/repository"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type DBlockerHandler struct {
 	Repo *repository.DBlockerRepository
+}
+
+func NewDBlockerHandler(repo *repository.DBlockerRepository) *DBlockerHandler {
+	return &DBlockerHandler{Repo: repo}
+}
+
+func (h *DBlockerHandler) CreateDBlocker(c *gin.Context) {
+	var input models.DBlocker
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.Repo.Create(&input); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data": input})
+}
+
+func (h *DBlockerHandler) GetDBlockers(c *gin.Context) {
+	dblockers, err := h.Repo.FindAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": dblockers})
+}
+
+func (h *DBlockerHandler) GetDBlockerByID(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := strconv.ParseUint(idParam, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	dblocker, err := h.Repo.FindByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "DBlocker not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": dblocker})
+}
+
+func (h *DBlockerHandler) UpdateDBlocker(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := strconv.ParseUint(idParam, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID format"})
+		return
+	}
+	var input models.DBlocker
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	input.ID = uint(id)
+
+	if err := h.Repo.Update(&input); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": input})
+}
+
+func (h *DBlockerHandler) DeleteDBlocker(c *gin.Context) {
+	idParam := c.Param("id")
+
+	id, err := strconv.ParseUint(idParam, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID format"})
+		return
+	}
+
+	if err := h.Repo.Delete(uint(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
 }
